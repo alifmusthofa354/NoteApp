bd62f24b7e86d80fec7c0bba1ff0f9cfa1900318
alifmusthofa354 <muhammad.alif.musthofa354@gmail.com>
2024-12-20 13:18:09 +0700

    url server hard

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -11,7 +11,7 @@ const Login = () => {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post("http://localhost:5000/login", {
+      const response = await axios.post("http://85.31.238.116:5001/login", {
         username,
         password,
       });
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -9,7 +9,7 @@ const Register = () => {
   const handleRegister = async (e) => {
     e.preventDefault();
     try {
-      await axios.post("http://localhost:5000/register", {
+      await axios.post("http://85.31.238.116:5001/register", {
         username,
         password,
       });
